420258bb53aab2568fa9281b8e3b4eb8f9f2b9c9
basel-abdelazim <basel.abdelazim@gmail.com>
2024-09-09 13:11:04 +0200

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar'; 
+import EventDetailsPage from './pages/EventDetailsPage';
+import SignUpPage from './pages/SignUpPage';
+import SignInPage from './pages/SignInPage';
+import CreateEventPage from './pages/CreateEventPage';
+import ProtectedRoute from './components/ProtectedRoute';
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Router>
+       {/* Navbar will be displayed on all pages */}
+       <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/events/:id" element={<EventDetailsPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/create-event" element={
+          <ProtectedRoute>
+            <CreateEventPage />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
