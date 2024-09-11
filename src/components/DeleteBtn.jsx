78a112674d15cfb54d8f64a5fc3c58d708214022
Twana Khudhur <twana.brno@gmail.com>
2024-09-12 00:26:51 +0200

    Handle Delete Event

--- src/components/DeleteBtn.jsx
+++ src/components/DeleteBtn.jsx
@@ -1,9 +1,48 @@
+import React, { useState } from "react";
 import { MdDelete } from "react-icons/md";
+import ConfirmDeleteModal from "./ConfirmDeleteModal";
+import HTTP from "../lib/HTTP";
+import { useToast } from "../context/ToastContext";
+import { useNavigate } from "react-router-dom";
 
-export default function DeleteBtn({ id }) {
+const DeleteBtn = ({ id }) => {
+  const { showToast } = useToast();
+  const [isModalOpen, setModalOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    try {
+      await HTTP(`/events/${id}`, { method: "DELETE" });
+
+      showToast("Event Deleted successfully!", "warning");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      
+      showToast(error.message, "error");
+      setError(error.message);
+    } finally {
+      setModalOpen(false);
+    }
+  };
+
   return (
-    <button className="bg-error hover:scale-105 rounded p-2">
-      <MdDelete />
-    </button>
+    <>
+      <button
+        onClick={() => setModalOpen(true)}
+        className="bg-error hover:scale-105 rounded p-2"
+        aria-label="Delete"
+      >
+        <MdDelete />
+      </button>
+
+      <ConfirmDeleteModal
+        isOpen={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        onConfirm={handleDelete}
+      />
+    </>
   );
-}
+};
+
+export default DeleteBtn;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,27 +1,30 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
+// src/components/Map.js
+
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 
 export default function Map({ event }) {
   const customMarkerIcon = new L.Icon({
-    iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
-    shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
     shadowSize: [41, 41],
   });
+  
   const latitude = event?.latitude ?? 0;
   const longitude = event?.longitude ?? 0;
+
   return (
     <>
-      {" "}
       {latitude && longitude ? (
-        <div className="mt-5">
+        <div className="mt-5 relative z-10">
           <MapContainer
             center={[latitude, longitude]}
             zoom={13}
-            style={{ height: "200px", width: "100%" }}
+            style={{ height: '200px', width: '100%' }}
           >
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
